3f92a493bbbaa2cb6368c5bba861169745ae4f05
rithienatan <rithienatan@gmail.com>
2021-01-22 11:16:56 -0300

    Finish bootstrap. Start JS.

---
+++ Others Stuffs/studies/Javascript/DataTypes.js
@@ -0,0 +1,19 @@
+console.log(typeof "qualquer coisa");
+console.log(typeof 31);
+console.log(typeof "31");
+
+console.log('');
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+console.log('');
+
+console.log(null === undefined);
+console.log(null == undefined);
+
+console.log('');
+
+console.log(typeof {name:'', age:35});
+console.log(typeof [1,2,3,4]);
+console.log(typeof function test() {});
---
+++ Others Stuffs/studies/Javascript/Operators.js
@@ -0,0 +1,10 @@
+let x = 10, y = 10;
+
+let exponetial = x ** y;
+let concat = x + y + "70"; //concat numbers type and strings
+
+if(x === y)
+{ console.log('Verify if are equals in type and value!'); }
+
+if(x !== y)
+{ console.log('Verify if not equal in type or value!'); }
